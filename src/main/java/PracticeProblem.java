c8d84f718b9971e35878d91d4f39d8beb5e674b3
TomL384 <350286454@tdsb.ca>
2026-03-27 15:28:40 +0000

    Final

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,39 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
+public static String isFive(int int1){
+	if (int1 == 5){
+		return "The number is Five" ;
+	}
+	else{
+		return"The number is not Five" ;
+	}
+}
 	
+public static String positiveOrNegative(double v1){
+	if (v1>0){
+		return"Positive" ;
+	}
+	else{
+		return "Negative" ;
+	}
+}
 
+public static String highOrLow (int int1){
+	if (int1 >100){
+		return"High" ;
+	}
+	else{
+		return"Low" ;
+	}
+}
+
+public static String isHello(String v1){
+	if (v1.equals("Hello")) {
+		return "The word is Hello" ;
+	}
+	else{
+		return"The word is not Hello" ;
+	}
+}
 }
